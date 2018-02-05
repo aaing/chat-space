af46673f48dd2f05cde392f0397653a92bdff0d4
yuta <yuta@yutas-MacBook-Air.local>
2018-02-05 13:08:57 +0900

    edit migration file to create members table

--- db/migrate/20180205040612_create_members.rb
+++ db/migrate/20180205040612_create_members.rb
@@ -1,7 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
-
+      t.integer :user_id, index: true, foreign_key: true
+      t.integer :group_id, index: true, foreign_key: true
       t.timestamps
     end
   end
