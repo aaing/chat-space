002b47923e6ec63da23b63f5f40bc2e5a6dd8e79
aaing <n.y1031@gmail.com>
2018-02-05 18:29:17 +0900

    edit create action and strong parameter

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,12 @@ class GroupsController < ApplicationController
   end
 
   def create
+    group = Group.new(group_params)
+    if group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,6 +23,7 @@ class GroupsController < ApplicationController
   private
 
   def group_params
+    params.require(:group).permit(:user_ids, :name)
   end
   
 end
