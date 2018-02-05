13e78ecd36df1c70076c6be4532aef7fd5b4459c
yuta <yuta@yutas-MacBook-Air.local>
2018-02-05 13:01:03 +0900

    set groups routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-
+  resources :groups, only: [:new, :edit, :show]
   root to: "messages#index"
 end
