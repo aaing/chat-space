581437735745f56c8b09d02cb5d1b3604b5b5533
yuta <yuta@yutas-MacBook-Air.local>
2018-02-05 13:44:30 +0900

    add empty actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,20 @@
 class GroupsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def group_params
+  end
+  
 end
