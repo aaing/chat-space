a384faebb3e09790aaa6d54bdfdf1bd80b80217d
yuta <yuta@yutas-MacBook-Air.local>
2018-02-05 13:38:35 +0900

    fix groups routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :edit, :show]
+  resources :groups, only: [:new, ,:create, :edit, :update]
   root to: "messages#index"
 end
