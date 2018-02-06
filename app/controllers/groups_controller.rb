db73b825f9faeca19915bb1ac18dc0ec1f4a08e5
aaing <n.y1031@gmail.com>
2018-02-06 12:55:34 +0900

    set error message in new view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,10 +5,11 @@ class GroupsController < ApplicationController
   end
 
   def create
-    group = Group.new(group_params)
-    if group.save
+    @group = Group.new(group_params)
+    if @group.save
       redirect_to root_path, notice: "グループを作成しました"
     else
+      @full_messages = @group.errors.full_messages
       render :new
     end
   end
