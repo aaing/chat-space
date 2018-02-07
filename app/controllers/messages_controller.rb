eb2203424161ad006459698f4caf386a5189b853
aaing <n.y1031@gmail.com>
2018-02-07 12:57:17 +0900

    edit create function and strong parameter

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,11 +6,14 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @group = Group.find(params[:group_id])
+    @group.messages.create(message_params)
   end
 
   private
 
   def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
   end
 
 end
