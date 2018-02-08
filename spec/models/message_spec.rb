d5d8d86ed4d8c972e7a9cd1e52a0a1f3dd1ee11e
aaing <n.y1031@gmail.com>
2018-02-08 18:37:11 +0900

    create message spec

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe Message, type: :model do
+  describe '#create' do
+
+    context 'can save' do
+
+      it "is valid with body" do
+        expect(build(:message, image: nil)).to be_valid
+      end
+
+      it "is valid with image" do
+        expect(build(:message, body: nil)).to be_valid
+      end
+
+      it "is valid with body and image" do
+        expect(build(:message)).to be_valid
+      end
+    end
+
+    context 'can not save' do
+
+      it "is invalid without body and image" do
+        message = build(:message, body: nil, image: nil)
+        message.valid?
+        binding.pry
+        expect(message.errors[:body]).to include("を入力してください")
+      end
+    end
+  end
+end
