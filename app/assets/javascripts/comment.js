31c119d52634fd0c0b4a45e4c1b0be17d3d9d0ab
aaing <n.y1031@gmail.com>
2018-02-11 18:15:48 +0900

    create comment js

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,39 @@
+$(function(){
+    function buildHTML(message){
+    var html = `<div class="content-message">
+                  <span class="content-message__user-name">
+                    ${message.body}
+                  </span>
+                  <img src="${message.image}">
+                  <span class="content-message__time-stamp">
+                    ${message.time_stamp}
+                  </span>
+                  <span class="content-message__text">
+                    ${message.name}
+                  </span>
+                </div>`
+    return html;
+  }
+  $(".new_message").on("submit", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = window.location.href
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.contents-main').append(html)
+      $('.message-form__text').val('')
+      $('#message_image').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
