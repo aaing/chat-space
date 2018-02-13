e9acde51cb6fabe9de6d156556a8079b324f7c30
aaing <n.y1031@gmail.com>
2018-02-13 18:38:17 +0900

    edit search.js

--- app/assets/javascripts/search_user.js
+++ app/assets/javascripts/search_user.js
@@ -1,17 +1,23 @@
 $(function(){
+  //検索結果のリスト
   var search_list = $(".user-search-result")
+
+  //インクリメンタルサーチされたuserのhtml生成
   function buildHTML(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
+    //末尾に追加
     search_list.append(html);
   }
 
+  //一致するユーザーが居ない場合メッセージを出すhtmlを生成
   function appendNoUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   ${ user }
                 </div>`
+    //末尾に追加
     search_list.append(html);
   }
 
@@ -38,5 +44,9 @@ $(function(){
     .fail(function() {
       alert('ユーザー検索に失敗しました');
     })
+  })
+
+  $(document).on("click", "a", function(){
+    console.log("www");
   })
 });
